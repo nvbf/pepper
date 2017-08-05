0d8037c8c474aea95572f31fcaea368795a317f0
Thor Even Tutturen <thor@bustbyte.no>
2017-08-05 14:16:55 +0200

    disable styled-components SSR

--- pages/_document.js
+++ pages/_document.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import Document, { Head, Main, NextScript } from 'next/document';
-import { ServerStyleSheet } from 'styled-components';
+// import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
   render() {
-    const sheet = new ServerStyleSheet();
-    const main = sheet.collectStyles(<Main />);
-    const styleTags = sheet.getStyleElement();
+    // const sheet = new ServerStyleSheet();
+    // const main = sheet.collectStyles(<Main />);
+    const main = <Main />;
+    // const styleTags = sheet.getStyleElement();
     return (
       <html lang="no">
         <Head>
           <title>Pepper</title>
           <meta name="viewport" content="initial-scale=1.0, width=device-width" />
           <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet" />
-          {styleTags}
+          {/* styleTags */}
         </Head>
         <body>
           <div className="root">
